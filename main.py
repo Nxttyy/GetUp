f72e918dfefb8641fc5f5967eed8b27f31ef322b
Natty <nutzaccs@gmail.com>
2022-05-01 19:55:15 +0300

    working animation and hide/unhide mechanism

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 pygame.init()
 
-width, height = 250, 100
+width, height = 160, 160
 
 screen = pygame.display.Info()
 x, y = screen.current_w, screen.current_h
@@ -13,6 +13,7 @@ settings = open('./settings.txt', 'r')
 
 #global variables
 noFrame = True
+window = None
 
 #reading settings
 for line in settings:
@@ -20,22 +21,50 @@ for line in settings:
     if line[0][0] != '\n' and  line[0][0] != '#':
         if line[0] == 'NoFrameDisplay':
             noFrame = bool(int(line[1][0]))
-print(noFrame)
 
-if noFrame:
-    flags = pygame.NOFRAME
-else:
-    flags = pygame.RESIZABLE 
+#sprite list
+sprites = ['./sprites/1.png', './sprites/2.png', './sprites/3.png', './sprites/4.png']
+spriteIndex = 0
 
-window = pygame.display.set_mode((width, height), flags, 0,0)
     
+def showWindow(prop):
+    global window
+
+    if noFrame and prop:
+        flags = pygame.NOFRAME|pygame.SHOWN
+    elif noFrame and not prop:
+        flags = pygame.NOFRAME|pygame.HIDDEN
+    elif not noFrame and prop:
+        flags = pygame.RESIZEABLE|pygame.SHOWN
+    else:
+        flags = pygame.RESIZEABLE|pygame.HIDDEN
+
+    window = pygame.display.set_mode((width, height), flags, 0,0)
+
+showWindow(False)
+pygame.display.set_caption('Get Up!')
+
 interval = 1 
 
 def main():
+    #gobals
+    global spriteIndex
+    global window
+
     then = datetime.now()
     totalThenMinutes = int((then.strftime('%H')))*60 + int(then.strftime('%M'))
 
+    clock = pygame.time.Clock()
     while True:
+
+        if spriteIndex < len(sprites)-1:
+            spriteIndex += 1
+        else:
+            spriteIndex = 0
+
+        img = pygame.image.load(sprites[spriteIndex])
+        img = pygame.transform.scale(img, (160,160))
+        window.blit(img, ((0,0)))
         #time
         now = datetime.now()
 
@@ -45,12 +74,13 @@ def main():
             print(str(totalNowMinutes) + 'Get Up Bro!')
             then = datetime.now()
             totalThenMinutes = int((then.strftime('%H')))*60 + int(then.strftime('%M'))
+            showWindow(True)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        window.fill((0,0,0))
+        clock.tick(5)
         pygame.display.flip()
 
 if __name__ == '__main__':
